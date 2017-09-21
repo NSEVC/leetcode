d9aa7029677a21ff7fb378f1be7fb4ab2c27af55
NSEVC <425237677@qq.com>
2017-09-21 22:00:40 +0800

    q24 swap nodes in pairs finished.

---
+++ q24/24swapnodes.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution{
+public:
+    ListNode* swapNodesInPairs(ListNode* head){
+        ListNode* first = new ListNode(-1);
+        ListNode* third = head->next;
+        ListNode* p = first;
+        ListNode* tmp = NULL;
+        while(head!=NULL and third!=NULL){
+            cout << head->val << " " << third->val << endl;
+            cout << "first->val: " << first->val << endl;
+            cout << "head->val: " << head->val << endl;
+            cout << "third->val: " << third->val << endl;
+            first->next = third;
+            tmp = third->next;
+            third->next = head;
+            first = head;
+            head = tmp;
+            cout << "tmp: " << tmp << endl;
+            if(tmp!=NULL) {third = tmp->next;}
+            else {third = NULL;}
+        }
+        first->next = head;
+
+        return p->next;
+    }
+};
+
+void push(ListNode* head, int val){
+    ListNode* p = new ListNode(val);
+    while(head->next != NULL){
+        head = head->next;
+    }
+    head->next = p;
+}
+
+void printList(ListNode* head){
+    while(head){
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    Solution s;
+    ListNode* head = new ListNode(1);
+    push(head, 2);
+    push(head, 3);
+    push(head, 4);
+    push(head, 5);
+    push(head, 6);
+    printList(head);
+
+    ListNode* result = s.swapNodesInPairs(head);
+    printList(result);
+
+    return 0;
+}
